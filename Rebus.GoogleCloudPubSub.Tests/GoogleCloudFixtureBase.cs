aba63fc433ee37e69f7d8bdcc18393e1fe7c5d17
Mogens Heller Grabe <mookid8000@gmail.com>
2021-03-26 12:35:59 +0100

    add mo stuff

---
+++ Rebus.GoogleCloudPubSub.Tests/GoogleCloudFixtureBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Grpc.Core;
+using Grpc.Core.Logging;
+using Rebus.Tests.Contracts;
+
+namespace Rebus.GoogleCloudPubSub.Tests
+{
+    public abstract class GoogleCloudFixtureBase : FixtureBase
+    {
+        static GoogleCloudFixtureBase() => GrpcEnvironment.SetLogger(new ConsoleLogger());
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+
+            var configurationFile = Path.Combine(AppContext.BaseDirectory, "google-cloud-credentials.json");
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", configurationFile);
+        }
+    }
+}
---
+++ Rebus.GoogleCloudPubSub.Tests/SpikeCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+using NUnit.Framework;
+using Rebus.Extensions;
+using Rebus.Messages;
+
+namespace Rebus.GoogleCloudPubSub.Tests
+{
+    [TestFixture]
+    public class SpikeCode : GoogleCloudFixtureBase
+    {
+        [Test]
+        public async Task CanDoThis()
+        {
+            var topicName = TopicName.FromProjectTopic("swift-citadel-308809", "test-topic");
+            var publisherClient = await PublisherClient.CreateAsync(topicName);
+
+            var pubsubMessage = new PubsubMessage
+            {
+                Attributes =
+                {
+                    {Headers.MessageId, Guid.NewGuid().ToString("n") },
+                    {Headers.ContentType, "application/json; charset=utf-8" },
+                    {Headers.Type, typeof(SpikeCode).GetSimpleAssemblyQualifiedName()},
+                },
+                Data = ByteString.CopyFrom(1, 2, 3, 4)
+            };
+
+            await publisherClient.PublishAsync(pubsubMessage);
+
+        }
+    }
+}
